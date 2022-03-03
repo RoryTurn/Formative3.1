5b2db4ec748430a8b6f6c327cedcd1820d25e4bd
rory <turnburory@gmail.com>
2022-03-03 14:54:11 +1300

    thurs one

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 console.log('poo');
 const key = '1a703a9bba8059208c952443967c3e8a';
  
-var term= "top-headlines";
+var query= "top-headlines?";
+var term= "topic=breaking-news"
 var nation= "country=nz";
 var max = "max=20";
+var language= "lang=en";
 
 
 
@@ -19,42 +21,41 @@ function countryChanger(x){
     if  (x === "1"){
     
 nation = "country=nz";
-var web = "https://gnews.io/api/v4/" + term + "?" + nation + "?" + max + "&token=" + key;
+var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
 console.log(nation); 
 console.log(web);
-// ajaxFunc(nation, web);   
+ajaxFunc(nation, web);   
     
 
     }
  if (x  === "2"){
         
         nation = "country=de";
-        var web = "https://gnews.io/api/v4/" + term + "?" + nation + "?" + max + "&token=" + key;
+        var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
         console.log(nation, web);
-        // ajaxFunc(nation, web);   
+        ajaxFunc(nation, web);   
         }
 if (x  === "3"){
         
         nation = "country=gb";
-        var web = "https://gnews.io/api/v4/" + term + "?" + nation + "?" + max + "&token=" + key;
+        var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
         console.log(nation, web);
-        // ajaxFunc(nation, web);   
+        ajaxFunc(nation, web);   
         }
  if (x  === "4"){
         
         nation = "country=jp"
-        var web = "https://gnews.io/api/v4/" + term + "?" + nation + "?" + max + "&token=" + key;
+        var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
         console.log(nation, web);
-        // ajaxFunc(nation, web);   
+        ajaxFunc(nation, web);   
 
         }
  if (x === "5"){
       
         nation = "country=us";
-        var web = "https://gnews.io/api/v4/" + term + "?" + nation + "?" + max + "&token=" + key;
-        
+        var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;        
         console.log(nation, web);
-        // ajaxFunc(nation, web);   
+        ajaxFunc(nation, web);   
         
 
         }
@@ -74,62 +75,72 @@ $('#categoryMenu li a').click(function(){
  
      if  (z === "1"){
      console.log("One")
-     term = "top-headlines";
-     
-
-     console.log(term, web);
- 
+     var query= "top-headlines?";
+     var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
+     ajaxFunc(query, web); 
      }
   if (z  === "2"){
          console.log("Two")
-         term = "world";
-         var web = "https://gnews.io/api/v4/" + term + "?" + nation + "?" + max + "&token=" + key;
-
-         console.log(term, web);
+         var query= "search?q=technology&/";
+         var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
+         ajaxFunc(query, web);          
          }
  if (z  === "3"){
          console.log("Three")
-         term = "nation";
-         var web = "https://gnews.io/api/v4/" + term + "?" + nation + "?" + max + "&token=" + key;
-
-         console.log(term, web);
+         query = "search?q=science&/";
+         var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
+         ajaxFunc(query, web);          
          }
   if (z  === "4"){
          console.log("Four")
-         term = "entertainment";
-         var web = "https://gnews.io/api/v4/" + term + "?" + nation + "?" + max + "&token=" + key;
-
-         console.log(term, web);
+         query = "search?q=entertainment&/";
+         var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
+         ajaxFunc(query, web);         
+         }
+  if (z  === "5"){
+         console.log("Five")
+         query = "search?q=health&/";
+         var web = "https://gnews.io/api/v4/" + query + nation + "?" + language + "?" + max + "&token=" + key;
+         ajaxFunc(query, web);          
          }
 
      
  };
-// function ajaxFunc(nation, web){
-// $.ajax({
-//     type: "GET",
-//     url: web,    
-//     success:  function (data){
+
+
+ function headerChangerCountry(){
+
+ }
+ function topicChangerCountry(){
         
-//         console.log(nation);
-//         console.log(web);
-//         console.log(data.articles[0].title);
-//         // $(example).empty().append(
-//         //     `
-//         //     <h1>Artice One: ${data.articles[0].title}</h1>
+}
+function ajaxFunc(nation, web){
+$.ajax({
+    type: "GET",
+    url: web,    
+    success:  function (data){
+        
+        console.log(nation);
+        console.log(query);
+        console.log(web);
+        console.log(data.articles[0].title);
+        // $(example).empty().append(
+        //     `
+        //     <h1>Artice One: ${data.articles[0].title}</h1>
 
-//         //     <p>Text: ${data.articles[0].description}</p>
-//         //     <p>Date: ${data.articles[0].publishedAt}</p>
-//         //     <img src="${data.articles[0].image}">
-//         //     <p>Origin: ${data.articles[0].source.name}</p>
+        //     <p>Text: ${data.articles[0].description}</p>
+        //     <p>Date: ${data.articles[0].publishedAt}</p>
+        //     <img src="${data.articles[0].image}">
+        //     <p>Origin: ${data.articles[0].source.name}</p>
 
-//         //     <p>Web Address: ${data.articles[0].source.url}</p>
+        //     <p>Web Address: ${data.articles[0].source.url}</p>
             
-//         //     `
+        //     `
 
-//         // );
+        // );
 
-//     }
+    }
 
-// });
+});
 
-// };
+};
